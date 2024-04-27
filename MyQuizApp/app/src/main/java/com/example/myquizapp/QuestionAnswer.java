ae4766cb47e6c9691f872e6b39b453318bd11cc9
mervemolo <merve_molo@hotmail.com>
2024-04-27 23:02:16 +0300

    Sınav Uygulaması
    Sınav hazırlama ve sınav  çözme uygulaması

---
+++ MyQuizApp/app/src/main/java/com/example/myquizapp/QuestionAnswer.java
@@ -0,0 +1,29 @@
+package com.example.myquizapp;
+
+public class QuestionAnswer {
+    public static String question[]={
+            "Which is the most spoken language in the world?",
+            "Which is not a programming language?",
+            "When does a loop run if its condition is true?",
+            "HTML is classified as what type of language?",
+            "What would a computer be without an operating system?"
+
+
+    };
+    public static String choices[][]={
+            {"Chinese", "Spanish", "English", "Turkish"},
+            {"Pascal","Basic","Html","Python","Cobol"},
+            {"When it is false","When it is true","Whenever the programmer wants","Randomly"},
+            {"Programming language","Operating system","Markup language","Network protocol"},
+            {"Does not work"," It works slowly","Can only perform certain tasks","It works with high performance"}
+    };
+    public static String correctAnswers[]={
+            "Chinese",
+            "Html",
+            "When it is true",
+            "Markup language",
+            "Does not work"
+
+
+    };
+}
